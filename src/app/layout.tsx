147822e44f897a691e2ea6b169ca04d87f177a44
berkayaskin <berkayaskiin@gmail.com>
2023-06-25 04:49:48 +0300

    add favicon, update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Producter - Product Management Tool',
+  title: 'Producter Task',
   description: 'The product management tool designed to become customer-driven',
 }
 
